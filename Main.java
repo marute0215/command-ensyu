93f5bbea8bcbcb1867b889bb3bc0dcb89957e868
takahashi_teruma <t.takahashi.sys22@morijyobi.ac.jp>
2022-11-08 12:08:46 +0900

    Main.javaの修正

--- Main.java
+++ Main.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.print("1人目の身長：");
         double heigt = s.nextDouble();
+        System.out.print("2人目の身長：");
+        double heigt2 = s.nextDouble();
+        System.out.print("1人目の体重：");
         double weight = s.nextDouble();
+        System.out.print("2人目の体重：");
+        double weight2 = s.nextDouble();
         String result = String.format("%.2f",weight/(heigt*heigt));
-        System.out.println("BMI:"+result);
+        System.out.println("１人目のBMI:"+result);
+        String result2 = String.format("%.2f",weight2/(heigt2*heigt2));
+        System.out.println("2人目のBMI:"+result2);
     }
     
 }
